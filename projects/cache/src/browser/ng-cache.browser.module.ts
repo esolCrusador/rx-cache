d060aac05a492d0226123516016f770faaa67367
Tamas <tbajzat@luware.com>
2020-03-27 16:38:47 +0100

    build fix

--- projects/cache/src/browser/ng-cache.browser.module.ts
+++ projects/cache/src/browser/ng-cache.browser.module.ts
@@ -4,21 +4,24 @@ import { CacheModule } from '../common/cache.module';
 import { CacheConfiguration } from '../common/cache.configuration';
 import { I_CACHE_LOGGER } from '../common/i-cache-logger';
 
+export function cacheConfigurationBrowserFactory() {
+  return new CacheConfiguration(CacheStoragesEnum.HYBRID, CacheStoragesEnum.SESSION_STORAGE, CacheStoragesEnum.MEMORY);
+}
+
 // @dynamic
 @NgModule({
-  imports: [CacheModule],
-  providers: [
-    {
-      provide: CacheConfiguration,
-      useValue: new CacheConfiguration(CacheStoragesEnum.HYBRID, CacheStoragesEnum.SESSION_STORAGE, CacheStoragesEnum.MEMORY),
-    },
-  ],
+  imports: [CacheModule]
 })
 export class NgCacheBrowserModule {
   public static forRoot(loggerType: any): ModuleWithProviders {
     return {
       ngModule: NgCacheBrowserModule,
-      providers: [{ provide: I_CACHE_LOGGER, useExisting: loggerType }],
+      providers: [
+        {
+          provide: CacheConfiguration,
+          useFactory: cacheConfigurationBrowserFactory
+        },
+        { provide: I_CACHE_LOGGER, useExisting: loggerType }],
     };
   }
 }
--- projects/cache/src/server/ng-cache.server.module.ts
+++ projects/cache/src/server/ng-cache.server.module.ts
@@ -4,23 +4,27 @@ import { CacheModule } from '../common/cache.module';
 import { CacheConfiguration } from '../common/cache.configuration';
 import { I_CACHE_LOGGER } from '../common/i-cache-logger';
 
+export function cacheConfigurationServerFactory() {
+  return new CacheConfiguration(CacheStoragesEnum.MEMORY);
+}
+
 // @dynamic
 @NgModule({
   imports: [
     CacheModule,
-  ],
-  providers: [
-    {
-      provide: CacheConfiguration,
-      useValue: new CacheConfiguration(CacheStoragesEnum.MEMORY),
-    },
-  ],
+  ]
 })
 export class NgCacheServerModule {
   public static forRoot(loggerType: any): ModuleWithProviders {
     return {
       ngModule: NgCacheServerModule,
-      providers: [{ provide: I_CACHE_LOGGER, useExisting: loggerType }]
+      providers: [
+        {
+          provide: CacheConfiguration,
+          useFactory: cacheConfigurationServerFactory,
+        },
+        { provide: I_CACHE_LOGGER, useExisting: loggerType }
+      ]
     };
   }
 }
